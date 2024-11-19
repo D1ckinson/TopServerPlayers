96ee456e001e1813b91ad6791446d334b8007c69
D1ckinson <124173489+D1ckinson@users.noreply.github.com>
2024-11-19 16:41:46 +0300

    1.0

--- TopServerPlayers/Program.cs
+++ TopServerPlayers/Program.cs
@@ -4,22 +4,118 @@ using System.Linq;
 
 namespace TopServerPlayers
 {
-    internal class Program
+    class Program
     {
         static void Main()
         {
-            Console.CursorVisible = false;
+            ServerFactory serverFactory = new ServerFactory();
+            Server server = serverFactory.Create();
 
+            server.Work();
+        }
+    }
+
+    class ServerFactory
+    {
+        private PlayerFactory _playerFactory = new PlayerFactory();
+
+        public Server Create() =>
+            new Server(_playerFactory.CreatePlayers());
+    }
+
+    class Server
+    {
+        private List<Player> _players;
+
+        public Server(List<Player> players)
+        {
+            _players = players;
+        }
+
+        public void Work()
+        {
+            const string SortByLevelCommand = "1";
+            const string SortByStrengthCommand = "2";
+            const string ExitCommand = "3";
+
+            int sortQuantity = 3;
+            bool isWork = true;
+            List<Player> players = _players;
+
+            while (isWork)
+            {
+                Console.WriteLine();
+                players.ForEach(player => player.ShowInfo());
+
+                Console.WriteLine($"\n" +
+                    $"{SortByLevelCommand} - вывести топ по уровню\n" +
+                    $"{SortByStrengthCommand} - вывести топ по силе\n" +
+                    $"{ExitCommand} - выйти\n");
+
+                switch (UserUtils.ReadString("Введите команду:"))
+                {
+                    case SortByLevelCommand:
+                        players = _players.OrderByDescending(player => player.Level).Take(sortQuantity).ToList();
+                        break;
+
+                    case SortByStrengthCommand:
+                        players = _players.OrderByDescending(player => player.Strength).Take(sortQuantity).ToList();
+                        break;
+
+                    case ExitCommand:
+                        isWork = false;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    class PlayerFactory
+    {
+        public List<Player> CreatePlayers()
+        {
             int playersQuantity = 10;
 
-            PlayersFabrik playersFabrik = new PlayersFabrik();
-            List<Player> players = playersFabrik.CreatePlayers(playersQuantity);
+            int[] levelStats = { 20, 60 };
+            int[] strengthStats = { 100, 400 };
 
-            ActionBuilder actionBuilder = new ActionBuilder(players);
-            Menu menu = new MainMenu(actionBuilder.GiveActions());
+            List<Player> players = new List<Player>();
 
-            menu.Work();
+            for (int i = 0; i < playersQuantity; i++)
+            {
+                string name = CreateName();
+                int level = UserUtils.GenerateStat(levelStats);
+                int strength = UserUtils.GenerateStat(strengthStats);
+
+                players.Add(new Player(name, level, strength));
+            }
+
+            return players;
         }
+
+        private string CreateName()
+        {
+            string name = UserUtils.GenerateRandomValue(GetNames());
+            int maxPrefix = 300;
+
+            name += UserUtils.GenerateInt(maxPrefix);
+
+            return name;
+        }
+
+        private List<string> GetNames() =>
+            new List<string>
+            {
+                "Геннадий",
+                "Дмитрий",
+                "Максим",
+                "Александр",
+                "Валерий",
+                "Михаил"
+            };
     }
 
     class Player
@@ -31,223 +127,50 @@ namespace TopServerPlayers
             Strength = strength;
         }
 
-        public string Name { get; private set; }
-        public int Level { get; private set; }
-        public int Strength { get; private set; }
+        public string Name { get; }
+        public int Level { get; }
+        public int Strength { get; }
 
+        public void ShowInfo()
+        {
+            const int ShortOffset = -5;
+            const int LongOffset = -15;
+
+            Console.WriteLine($"{Name,LongOffset} Уровень: {Level,ShortOffset} Сила: {Strength}");
+        }
     }
 
-    class PlayersFabrik
+    static class UserUtils
     {
-        private List<string> _names;
+        private static Random s_random = new Random();
 
-        private int[] _levelStats = { 20, 60 };
-        private int[] _strengthStats = { 100, 400 };
-
-        private Random _random = new Random();
-
-        public PlayersFabrik() =>
-            FillNames();
-
-        public List<Player> CreatePlayers(int playersQuantity)
+        public static string ReadString(string text)
         {
-            List<Player> players = new List<Player>();
+            Console.Write(text);
 
-            for (int i = 0; i < playersQuantity; i++)
+            return Console.ReadLine();
+        }
+
+        public static T GenerateRandomValue<T>(IEnumerable<T> values)
+        {
+            int index = s_random.Next(values.Count());
+
+            return values.ElementAt(index);
+        }
+
+        public static int GenerateStat(int[] stats)
+        {
+            int maxLength = 2;
+
+            if (stats.Length != maxLength)
             {
-                string name;
-                int maxNameNumber = 300;
-
-                if (i >= _names.Count)
-                {
-                    int index = i % _names.Count;
-                    name = _names[index] + _random.Next(maxNameNumber + 1);
-                }
-                else
-                {
-                    name = _names[i];
-                }
-
-                int level = _random.Next(_levelStats[0], _levelStats[1]);
-                int strength = _random.Next(_strengthStats[0], _strengthStats[1]);
-
-                players.Add(new Player(name, level, strength));
+                throw new ArgumentException("Массив stats должен содержать ровно 2 элемента.");
             }
 
-            return players;
+            return s_random.Next(stats[0], stats[1] + 1);
         }
 
-        private void FillNames() =>
-            _names = new List<string>
-            {
-                "Геннадий",
-                "Дмитрий",
-                "Максим",
-                "Александр",
-                "Валерий",
-                "Михаил"
-            };
-    }
-
-    class MainMenu : Menu
-    {
-        private Dictionary<string, Action> _actions = new Dictionary<string, Action>();
-
-        public MainMenu(Dictionary<string, Action> actions)
-        {
-            _actions = actions;
-            _actions.Add("Выход", Exit);
-            _items = _actions.Keys.ToArray();
-        }
-
-        protected override void ConfirmActionSelection()
-        {
-            base.ConfirmActionSelection();
-
-            _actions[_items[_itemIndex]].Invoke();
-        }
-    }
-
-    abstract class Menu
-    {
-        private const ConsoleKey MoveSelectionUp = ConsoleKey.UpArrow;
-        private const ConsoleKey MoveSelectionDown = ConsoleKey.DownArrow;
-        private const ConsoleKey ConfirmSelection = ConsoleKey.Enter;
-
-        private ConsoleColor _backgroundColor = ConsoleColor.White;
-        private ConsoleColor _foregroundColor = ConsoleColor.Black;
-
-        protected string[] _items;
-        protected int _itemIndex = 0;
-
-        private bool _isRunning;
-
-        public void Work()
-        {
-            _isRunning = true;
-
-            while (_isRunning)
-            {
-                DrawItems();
-
-                ReadKey();
-            }
-        }
-
-        protected virtual void ConfirmActionSelection() =>
-            EraseText();
-
-        protected void Exit() =>
-            _isRunning = false;
-
-        private void SetItemIndex(int index)
-        {
-            int lastIndex = _items.Length - 1;
-
-            if (index > lastIndex)
-                index = lastIndex;
-
-            if (index < 0)
-                index = 0;
-
-            _itemIndex = index;
-        }
-
-        private void ReadKey()
-        {
-            switch (Console.ReadKey(true).Key)
-            {
-                case MoveSelectionDown:
-                    SetItemIndex(_itemIndex + 1);
-                    break;
-
-                case MoveSelectionUp:
-                    SetItemIndex(_itemIndex - 1);
-                    break;
-
-                case ConfirmSelection:
-                    ConfirmActionSelection();
-                    break;
-            }
-        }
-
-        private void DrawItems()
-        {
-            Console.SetCursorPosition(0, 0);
-
-            for (int i = 0; i < _items.Length; i++)
-                if (i == _itemIndex)
-                    WriteColoredText(_items[i]);
-                else
-                    Console.WriteLine(_items[i]);
-        }
-
-        private void WriteColoredText(string text)
-        {
-            Console.ForegroundColor = _foregroundColor;
-            Console.BackgroundColor = _backgroundColor;
-
-            Console.WriteLine(text);
-
-            Console.ResetColor();
-        }
-
-        private void EraseText()
-        {
-            int spaceLineSize = 60;
-            char space = ' ';
-
-            Console.SetCursorPosition(0, 0);
-
-            for (int i = 0; i < _items.Length; i++)
-                Console.WriteLine(new string(space, spaceLineSize));
-        }
-    }
-
-    class ActionBuilder
-    {
-        private int _topQuantity = 3;
-        private int _cursorPositionYIndent = 2;
-        private List<Player> _players;
-
-        public ActionBuilder(List<Player> players)
-        {
-            _players = players;
-
-            WriteAllPlayers();
-        }
-
-        public Dictionary<string, Action> GiveActions() =>
-            new Dictionary<string, Action>
-            {
-                { "Вывести всех игроков", WriteAllPlayers },
-                { $"Вывести топ {_topQuantity} игроков по уровню", WriteTopLevelPlayers },
-                { $"Вывести топ {_topQuantity} игроков по силе", WriteTopStrengthPlayers }
-            };
-
-        private void WritePlayers(IEnumerable<Player> players)
-        {
-            int spaceLineSize = 40;
-            char space = ' ';
-
-            Console.SetCursorPosition(0, GiveActions().Count + _cursorPositionYIndent);
-
-            for (int i = 0; i < _players.Count; i++)
-                Console.WriteLine(new string(space, spaceLineSize));
-
-            Console.SetCursorPosition(0, GiveActions().Count + _cursorPositionYIndent);
-
-            foreach (Player player in players)
-                Console.WriteLine($"{player.Name}, уровень - {player.Level}, сила - {player.Strength}");
-        }
-
-        private void WriteAllPlayers() =>
-            WritePlayers(_players);
-
-        private void WriteTopStrengthPlayers() =>
-            WritePlayers(_players.OrderByDescending(player => player.Strength).Take(_topQuantity));
-
-        private void WriteTopLevelPlayers() =>
-            WritePlayers(_players.OrderByDescending(player => player.Level).Take(_topQuantity));
+        public static int GenerateInt(int max) =>
+            s_random.Next(max);
     }
 }
